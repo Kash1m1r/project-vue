006da169bdf282393aacb40d99ad6303615f08e9
Kash1m1r <adm1nkash1m1r@gmail.com>
2022-01-26 19:27:54 -0300

    mudançasafetivas

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="corpo">
+    <router-view></router-view>
+  </div>
+</template> 
+
+<script>
+export default {
+     
+}
+
+</script>
+
+<style>
+  .corpo{
+    font-family: Helvetica, sans-serif;
+    width: 100%;
+    margin: 0 auto;
+  }
+</style>
